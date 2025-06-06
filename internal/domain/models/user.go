159f0a1e5c083ff4ffa1a6163b88877c1f9543ec
Manuel Carbajal <manicar2093@gmail.com>
2025-06-05 21:43:17 -0600

    feat: add default to created at to take database default now()

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -10,5 +10,6 @@ type User struct {
 	Id        uuid.UUID `gorm:"default:gen_random_uuid()" json:"id" mapstructure:"id" param:"id"`
 	Email     string    `json:"email" mapstructure:"email" param:"email" form:"email" validate:"required"`
 	Password  string    `json:"password" mapstructure:"password" param:"password" form:"password" validate:"required|min_len:10"`
-	CreatedAt time.Time `json:"created_at" mapstructure:"created_at" param:"created_at" form:"created_at"`
+	CreatedAt time.Time `gorm:"default:now()" json:"created_at" mapstructure:"created_at" param:"created_at" form:"created_at"`
+	Status    Status    `json:"status" mapstructure:"status" param:"status" form:"status" validate:"required|in:active,deactived,pending"`
 }
